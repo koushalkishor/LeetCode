6f49a7e29bb5259b442f8bad1a2e1edb86d5412e
Koushal kishor <68542676+koushalkishor@users.noreply.github.com>
2022-03-28 21:38:35 +0530

    Time: 2 ms (80.91%), Space: 48.4 MB (26.50%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -2,7 +2,8 @@ class Solution {
     public int[] countBits(int n) {
         int[] output=new int[n+1];
         for(int i=0;i<output.length;i++){
-            output[i]=output[i>>1]+i%2;
+            //output[i]=output[i>>1]+i%2;
+            output[i]=output[i/2]+i%2;
         }
         return output;
     }
