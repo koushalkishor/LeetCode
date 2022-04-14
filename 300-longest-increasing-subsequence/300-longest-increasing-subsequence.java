7c240316c1a0f1cb6935847a357fda88515cb727
Koushal kishor <68542676+koushalkishor@users.noreply.github.com>
2022-04-14 17:28:03 +0530

    Time: 55 ms (64.81%), Space: 41.7 MB (92.70%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,28 +1,46 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
+        //one approach
+//         int n=nums.length;
+//         int[] dp=new int[n];
+//         int i,j; // where i is called leading pointer and j is called trailing pointer
+//         int max=0;
+//         for(i=0;i<n;i++){
+//             dp[i]=1;
+//         }
+        
+//         for(i=1;i<n;i++){
+//             for(j=0;j<i;j++){
+//                 if(nums[j]<nums[i]){
+//                     dp[i]=Math.max(dp[i],dp[j]+1);
+//                     //max++;
+//                 }
+//             }
+//             //max=0;
+//         }
+//         for(i=0;i<n;i++){
+//             if(max<dp[i]){
+//                 max=dp[i];
+//             }
+//         }
+        
+//         return max;
         int n=nums.length;
         int[] dp=new int[n];
         int i,j;
-        int max=0;
-        for(i=0;i<n;i++){
-            dp[i]=1;
-        }
+        dp[0]=1;
         
+        int ans=1;
         for(i=1;i<n;i++){
-            for(j=0;j<i;j++){
+            int max=0;
+            for(j=0;j<=i;j++){
                 if(nums[j]<nums[i]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                    //max++;
+                    max=Math.max(max,dp[j]);
                 }
             }
-            //max=0;
-        }
-        for(i=0;i<n;i++){
-            if(max<dp[i]){
-                max=dp[i];
+            dp[i]=max+1;
+            ans=Math.max(ans,dp[i]);
             }
+        return ans;
         }
-        
-        return max;
     }
-}
