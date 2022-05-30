22bb66c339f7536f642a0c534edda234711489f5
Koushal kishor <68542676+koushalkishor@users.noreply.github.com>
2022-05-30 21:41:20 +0530

    Time: 1 ms (87.65%), Space: 48.7 MB (98.07%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n=s.length;
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
+}
