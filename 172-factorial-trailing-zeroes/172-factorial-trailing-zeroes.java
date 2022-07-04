543e8db3dc0e57c3eea3a7d3f4791f8b8482d7b7
Koushal kishor <68542676+koushalkishor@users.noreply.github.com>
2022-07-04 21:06:31 +0530

    Time: 1 ms (86.18%), Space: 40.8 MB (69.61%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
@@ -1,17 +1,9 @@
 class Solution {
     public int trailingZeroes(int n) {
-        // if(n<5) 
-        // {
-        //     return 0;
-        // }
-        // return n/5 + trailingZeroes(n/5);
-        
-        // another method
         int count=0;
         while(n>0){
             n=n/5;
             count+=n;
-            
         }
         return count;
     }
