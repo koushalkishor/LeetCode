2e5e74d6cc142b174c3ad9ec7b60b1eddb72a952
Koushal kishor <68542676+koushalkishor@users.noreply.github.com>
2022-07-08 21:52:59 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (38.50%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -10,29 +10,61 @@
  * }
  */
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        ListNode t=head;
-       ListNode fast=head;
-        ListNode  slow=head;
-        int flag=0;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-            if(slow==fast){
-                flag=1;
-                break;
-            }
-        }
-        if(flag==1){
-            
-            while(t!=slow){
-                t=t.next;
-                slow=slow.next;
-            }
-        }
-        else{
-            return null;
-        }
-        return t;
-    }
+//     static int flag;
+ public ListNode detectCycle(ListNode head) {
+//         ListNode temp=cycle(head);
+//         ListNode ram=head;
+//        if(flag==1){
+//         while(ram!=temp){
+//             temp=temp.next;
+//             ram=ram.next;
+//         }
+//        }
+//         else{
+//             return null;
+//         }
+//         return ram;
+//     }
+//     public ListNode cycle(ListNode head){
+//         ListNode tem=head;
+//         ListNode fast=head;
+//         while(fast!=null && fast.next!=null){
+//             tem=tem.next;
+//             fast=fast.next.next;
+//             if(tem==fast){
+//                 flag=1;
+//                 return tem;
+               
+//             }
+//         }
+//         return null;
+     
+     
+     ListNode temp=head;
+     ListNode fast=head;
+     int flag=0;
+     
+     while(fast!=null && fast.next!=null){
+        temp=temp.next;
+         fast=fast.next.next;
+         if(temp==fast){
+             flag=1;
+             break;
+         }
+     }
+     
+     ListNode slow=head;
+     if(flag==1){
+         while(slow!=temp){
+             slow=slow.next;
+             temp=temp.next;
+         }
+         return temp;
+     }
+         else{
+             return null;
+         }
+    
+     }
+    
 }
