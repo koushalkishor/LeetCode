052ffa2329aeee5c0b91a707385744c6109d8a6a
Koushal kishor <68542676+koushalkishor@users.noreply.github.com>
2022-07-09 15:52:33 +0530

    Time: 13 ms (6.07%), Space: 42.3 MB (13.51%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        if(head==null){
+            return 0;
+        }
+        String str="";
+        int dec=0;
+        
+        ListNode temp=head;
+        while(temp!=null){
+            str+=temp.val;
+            temp=temp.next;
+        }
+        int num=Integer.parseInt(str,2);
+        // int temp=num;
+        // while(num>0)
+        return num;
+    }
+}
